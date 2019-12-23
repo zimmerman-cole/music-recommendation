a33915259ad173d7ca793ed2a31cfd771616fadc
Cole Zimmerman <czimmer112@gmail.com>
2019-12-23 16:23:26 -0600

    Update constants.py w/ MSD metadata
    - Add class 'src.constants.MSDMetadata' containing metadata on the Million Songs Dataset

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,16 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = os.path.abspath(os.path.join(ROOT_PATH, '../'))
 DATA_PATH = os.path.join(ROOT_PATH, 'data/')
+
+
+class MSDMetadata(object):
+    """
+    Metadata for the Million Songs Dataset.
+    
+    - See http://millionsongdataset.com/ for details.
+    - Data was sourced from Kaggle @ https://www.kaggle.com/c/msdchallenge/data.
+    """
+    columns = ['user_id', 'song_id', 'num_plays']
+    num_training_points = 48373586
+    num_unique_users = 110000
+    num_unique_songs = 386213
