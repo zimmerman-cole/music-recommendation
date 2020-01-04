590946c7ca55edc79c32da65f248e5040f700ec0
Cole Zimmerman <czimmer112@gmail.com>
2020-01-04 16:29:23 -0600

    Create rec/msdutils.py
    - Create rec/msdutils.py, a module for working with Million Song Dataset data.
    - Move methods `load_song_ids()`, `load_user_ids()`, and `load_song_to_track_data()` to rec/msdutils.py

--- rec/data_loader.py
+++ rec/data_loader.py
@@ -11,14 +11,6 @@ import torch
 from .constants import DATA_PATH, MSDMetadata
 
 
-# The filenames of each data file
-_filenames = {
-    'songs':         'msd/kaggle_songs.txt',
-    'users':         'msd/kaggle_users.txt',
-    'song_to_track': 'msd/taste_profile_song_to_tracks.txt',
-}
-
-
 class _DataPathFetcher(object):
     
     _valid = ['train', 'valid', 'test']
@@ -215,110 +207,7 @@ class Dataset(torch.utils.data.Dataset):
         yield (current_user_id, np.array(current_user_data).reshape(-1, 2))
 
 
-def load_song_ids() -> pd.DataFrame:
-    """
-    Load the mapping from song ID associated with each song into a pandas.DataFrame.
-    
-    Args:
-    ===========================
-    None
-    ===========================
-    
-    Returns:
-    ===========================
-    (pd.DataFrame) song_ids:
-    * A pandas DataFrame containing the song IDs (in the sole column 'song_id').
-    ===========================
-    """
-    path = os.path.join(DATA_PATH, _filenames['songs'])
-    
-    song_ids = pd.read_csv(
-        path, header=None, names=['song_id', 'id'], sep=' ', index_col=1
-    )
-    
-    return song_ids
 
-
-def load_user_ids() -> pd.DataFrame:
-    """
-    Load the ID associated with each user into a pandas.DataFrame.
-    
-    Args:
-    ===========================
-    None
-    ===========================
-    
-    Returns:
-    ===========================
-    (pd.DataFrame) user_ids:
-    * A pandas DataFrame containing the user IDs (in the sole column 'user_id').
-    ===========================
-    """
-    path = os.path.join(DATA_PATH, _filenames['users'])
-    
-    user_ids = pd.read_csv(path, header=None, names=['user_id'])
-    
-    return user_ids
-
-
-def load_song_to_track_data() -> pd.DataFrame:
-    """
-    Loads the data mapping song IDs to track IDs.
-    
-    Args:
-    ===========================
-    None
-    ===========================
-    
-    Returns:
-    ===========================
-    (pd.DataFrame) song_to_track:
-    * A pandas DataFrame containing the mappings from song ID to track ID(s).
-    ===========================
-    """
-    path = os.path.join(DATA_PATH, _filenames['song_to_track'])
-    
-    song_to_track = []
-    with open(path, 'r') as text_file:
-        for line in text_file:
-            line = line.strip().split('\t')
-            song, tracks = line[0], line[1:]
-            
-            song_to_track.append([song, tracks])
-    
-    song_to_track = pd.DataFrame(song_to_track, columns=['song_id', 'track_ids'])
-    
-    return song_to_track
-
-
-# def load_track_info() -> pd.DataFrame:
-#     """
-#     Loads information 
-    
-#     Args:
-#     ===========================
-#     None
-#     ===========================
-    
-#     Returns:
-#     ===========================
-#     (pd.DataFrame) track_info:
-#     * 
-#     ===========================
-#     """
-#     path = os.path.join(DATA_PATH, _filenames['song_to_track'])
-    
-#     track_info = []
-#     with open(path, 'r') as text_file:
-#         for line in text_file:
-#             line = line.strip().split('<SEP>')
-#             song_id, 
-            
-#             track_info.append([song, tracks])
-    
-#     track_info = pd.DataFrame(track_info, columns=['song_id', 'track_ids'])
-    
-#     return track_info
 
 
 
---
+++ rec/msdutils.py
@@ -0,0 +1,104 @@
+"""
+Module for managing Million Song Database (MSD) data.
+"""
+import os
+
+import pandas as pd
+
+from rec.data_loader import load_song_ids
+
+# The filenames of each data file
+_filenames = {
+    'songs':         'msd/kaggle_songs.txt',
+    'users':         'msd/kaggle_users.txt',
+    'song_to_track': 'msd/taste_profile_song_to_tracks.txt',
+}
+
+
+def load_song_ids() -> pd.DataFrame:
+    """
+    Load the mapping from song ID associated with each song into a pandas.DataFrame.
+    
+    Args:
+    ===========================
+    None
+    ===========================
+    
+    Returns:
+    ===========================
+    (pd.DataFrame) song_ids:
+    * A pandas DataFrame containing the song IDs (in the sole column 'song_id').
+    ===========================
+    """
+    path = os.path.join(DATA_PATH, _filenames['songs'])
+    
+    song_ids = pd.read_csv(
+        path, header=None, names=['song_id', 'id'], sep=' ', index_col=1
+    )
+    
+    return song_ids
+
+
+def load_user_ids() -> pd.DataFrame:
+    """
+    Load the ID associated with each user into a pandas.DataFrame.
+    
+    Args:
+    ===========================
+    None
+    ===========================
+    
+    Returns:
+    ===========================
+    (pd.DataFrame) user_ids:
+    * A pandas DataFrame containing the user IDs (in the sole column 'user_id').
+    ===========================
+    """
+    path = os.path.join(DATA_PATH, _filenames['users'])
+    
+    user_ids = pd.read_csv(path, header=None, names=['user_id'])
+    
+    return user_ids
+
+
+def load_song_to_track_data() -> pd.DataFrame:
+    """
+    Loads the data mapping song IDs to track IDs.
+    
+    Args:
+    ===========================
+    None
+    ===========================
+    
+    Returns:
+    ===========================
+    (pd.DataFrame) song_to_track:
+    * A pandas DataFrame containing the mappings from song ID to track ID(s).
+    ===========================
+    """
+    path = os.path.join(DATA_PATH, _filenames['song_to_track'])
+    
+    song_to_track = []
+    with open(path, 'r') as text_file:
+        for line in text_file:
+            line = line.strip().split('\t')
+            song, tracks = line[0], line[1:]
+            
+            song_to_track.append([song, tracks])
+    
+    song_to_track = pd.DataFrame(song_to_track, columns=['song_id', 'track_ids'])
+    
+    return song_to_track
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
